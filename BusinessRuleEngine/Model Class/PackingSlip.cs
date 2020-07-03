d0a1036e1419df168f30f12ba07554d1b0e92705
Mani Shankar <mani.shankar@MediaKind.Com>
2020-07-03 09:44:01 +0530

    Added basic model class with minmal logging

---
+++ BusinessRuleEngine/Model Class/PackingSlip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BusinessRulesEngine
+{
+    public class PackingSlip : Item
+    {
+        public DateTime PurchaseDate { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public virtual bool Perform()
+        {
+            try
+            {
+                Helper.PerformResultAction(this, "generated packing slip for");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Helper.LogException(ex);
+                return false;
+            }
+
+        }
+
+    }
+
+
+}
